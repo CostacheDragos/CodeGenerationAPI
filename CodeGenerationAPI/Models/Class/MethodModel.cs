32cbbee17aa483e3410d5904474a564453457738
Dragos Costache <dcostache01@gmail.com>
2023-03-31 17:20:01 +0300

    Parameters passed to functions can be reference or value and the const keyword can be added

--- CodeGenerationAPI/Models/Class/MethodModel.cs
+++ CodeGenerationAPI/Models/Class/MethodModel.cs
@@ -11,7 +11,7 @@ namespace CodeGenerationAPI.Models.Class
         public string Name { get; set; } = string.Empty;
         public string AccessModifier { get; set; } = string.Empty;
         public string ReturnType { get; set; } = string.Empty;
-        public List<PropertyModel>? Parameters { get; set; }
+        public List<ParameterModel>? Parameters { get; set; }
         public bool IsVirtual { get; set; } = false;
     }
 }
---
+++ CodeGenerationAPI/Models/Class/ParameterModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeGenerationAPI.Models.Class
+{
+    public class ParameterModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public bool IsRef { get; set; } = false;
+        public bool IsConst { get; set; } = false;
+    }
+}
