ddc7f8d22eeeaefa81b7a77941390ed5da6a9f92
Dragos Costache <dcostache01@gmail.com>
2023-04-03 15:19:22 +0300

    Data types now support multiple pointers, each having an option to be const

--- CodeGenerationAPI/Models/Class/DataTypeModel.cs
+++ CodeGenerationAPI/Models/Class/DataTypeModel.cs
@@ -4,6 +4,11 @@
     {
         public string Name { get; set; } = string.Empty;
         public bool IsConst { get; set; } = false;
-        public bool IsPointer { get; set; } = false;
+        public List<Pointer>? PointerList { get; set; }
+
+        public class Pointer
+        {
+            public bool IsConst { get; set; } = false;
+        }
     }
 }
